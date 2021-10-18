a529adcb48690349d2089f3c49e707f56bb34b98
User <belpc2012@gmail.com>
2021-10-18 17:55:44 +0300

    task

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,14 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+   constructor(){
+    this._size = 0;
+    this._storage = {};
+   }
+  push(data) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    
+      // increases the size of our storage
+      var size = this._size++;
+   
+      // assigns size as a key of storage
+      // assigns data as the value of this key
+      this._storage[size] = data;
+  
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    var size = this._size,
+        deletedData;
+ 
+    deletedData = this._storage[size];
+ 
+    delete this._storage[size];
+    this.size--;
+ 
+    return deletedData;
   }
 
   peek() {
